b9fd402aa7e5085841ab6d3ca5fcc566f7efcd1d
synj <matthew.gregory@hotmail.co.uk>
2017-05-23 17:35:57 +0100

    Clean up

--- picker.js
+++ picker.js
@@ -1,13 +1,3 @@
-// # Used to track colour changes
-// colorPicker.addEventListener("input", updateFirst, false);
-// colorPicker.addEventListener("change", watchColorPicker, false);
-
-// function watchColorPicker(event) {
-//   document.querySelectorAll("p").forEach(function(p) {
-//     p.style.color = event.target.value;
-//   });
-// }
-
 var colour_1
 var defaultColour = "#c0ffee";
 
